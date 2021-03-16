01db9da2ad76924e556ed63616ff2139fde7359d
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-03-16 10:32:40 +0100

    Add Footer component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import "./App.scss";
 
 import Header from "./components/header/Header";
 import Content from "./components/content/Content";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 };
