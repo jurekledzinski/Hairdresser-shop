edf1edf9675c2acb79728ac92e50467faf44e33b
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-03-19 18:41:20 +0100

    Add MainPage component to gather all main instances of components to later use this component in router on path homepage

---
+++ client/src/components/mainpage/MainPage.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+import Header from "../header/Header";
+import Content from "../content/Content";
+import Footer from "../footer/Footer";
+
+const MainPage = () => {
+  return (
+    <Fragment>
+      <Header />
+      <Content />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default MainPage;
