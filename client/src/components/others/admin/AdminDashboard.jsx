2f0bb2d68a2b09a52d277acfc8801affb61ed68e
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-03-20 22:02:46 +0100

    Create component AdminDashboard

---
+++ client/src/components/others/admin/AdminDashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./AdminDashboard.scss";
+
+const AdminDashboard = () => {
+  return (
+    <section className="admin">
+      <div className="admin__wrapper">
+        <div className="admin__left"></div>
+        <div className="admin__right"></div>
+      </div>
+    </section>
+  );
+};
+
+export default AdminDashboard;
