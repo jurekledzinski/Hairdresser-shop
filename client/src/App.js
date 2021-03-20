3bd80bbd500451dabd2666de4836e7129695a94f
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-03-20 22:01:47 +0100

    Add routes admin login register

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import MainPage from "./components/mainpage/MainPage";
 import BookingMainPage from "./components/others/booking/BookingMainPage";
 import TeamDetails from "./components/content/team/TeamDetails";
 import BookingTermPolicyDetails from "./components/others/booking/BookingTermPolicyDetails";
+import AdminDashboard from "./components/others/admin/AdminDashboard";
+import Login from "./components/others/login/Login";
+import Register from "./components/others/register/Register";
 
 const App = () => {
   return (
@@ -17,6 +20,9 @@ const App = () => {
           <Route path="/booking" component={BookingMainPage} />
           <Route path="/team-details" component={TeamDetails} />
           <Route path="/term-policy" component={BookingTermPolicyDetails} />
+          <Route path="/admin" component={AdminDashboard} />
+          <Route path="/login-admin" component={Login} />
+          <Route path="/register-admin" component={Register} />
         </Switch>
       </Router>
     </div>
