4dcb27599cd1d18eb21d799ecf379f1ead528726
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-03-30 15:32:12 +0200

    Create function to handle submit boking form

--- client/src/components/others/booking/BookingForm.jsx
+++ client/src/components/others/booking/BookingForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import "./BookingForm.scss";
 
@@ -8,8 +9,14 @@ import BookingFormChooseService from "./BookingFormChooseService";
 import BookingFormNameEmailPhone from "./BookingFormNameEmailPhone";
 
 const BookingForm = () => {
+  const history = useHistory();
+  const handleBookingForm = (e) => {
+    e.preventDefault();
+    const id = "123fkgsehddsfrt342dsf";
+  };
+
   return (
-    <form className="booking__form">
+    <form className="booking__form" onSubmit={handleBookingForm}>
       <BookingFormChooseHairDresser />
       <BookingFormDayTime />
       <BookingFormChooseService />
