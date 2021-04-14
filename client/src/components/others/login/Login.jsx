bca2fcaae0efd2e5b8fa6af0e1a4b063dae84980
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-14 07:51:14 +0200

    Add Formik and yup to Login and Register components

--- client/src/components/others/login/Login.jsx
+++ client/src/components/others/login/Login.jsx
@@ -1,53 +1,98 @@
 import React from "react";
 
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 import "./Login.scss";
 
 import ButtonGoBackLoginRegister from "../buttonGoBackLoginRegister/ButtonGoBackLoginRegister";
 
 const Login = () => {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid email").required("Email required"),
+    password: Yup.string().required("Password required"),
+  });
+
+  const onSubmit = (values, submitProps) => {
+    console.log(values);
+    console.log(submitProps);
+    submitProps.resetForm();
+  };
+
+  const errorMsg = (props) => {
+    return <p className="login__error-msg">{props.children}</p>;
+  };
+
   return (
-    <section className="login">
-      <div className="login__wrapper">
-        <ButtonGoBackLoginRegister />
-        <div className="login__left">
-          <span className="login__cover-left"></span>
-          <div className="login__left-redirects">
-            <h2 className="login__left-title">Log in</h2>
-            <p className="login__left-subtitle">
-              Please fill in your credentials to enter into admin dashboard
-            </p>
-          </div>
-        </div>
-        <div className="login__right">
-          <h2 className="login__right-title">Log in</h2>
-          <form className="login__form">
-            <div className="login__input-wrapper">
-              <label className="login__label">Email</label>
-              <input
-                className="login__input"
-                type="email"
-                placeholder="Email"
-              />
-              <span className="login__icon">
-                <i className="far fa-envelope"></i>
-              </span>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {(formik) => {
+        console.log(formik);
+        return (
+          <section className="login">
+            <div className="login__wrapper">
+              <ButtonGoBackLoginRegister />
+              <div className="login__left">
+                <span className="login__cover-left"></span>
+                <div className="login__left-redirects">
+                  <h2 className="login__left-title">Log in</h2>
+                  <p className="login__left-subtitle">
+                    Please fill in your credentials to enter into admin
+                    dashboard
+                  </p>
+                </div>
+              </div>
+              <div className="login__right">
+                <h2 className="login__right-title">Log in</h2>
+                <Form className="login__form">
+                  <ErrorMessage name="email" component={errorMsg} />
+                  <div className="login__input-wrapper">
+                    <label className="login__label">Email</label>
+                    <Field
+                      className="login__input"
+                      name="email"
+                      type="email"
+                      placeholder="Your email"
+                    />
+                    <span className="login__icon">
+                      <i className="far fa-envelope"></i>
+                    </span>
+                  </div>
+                  <ErrorMessage name="password" component={errorMsg} />
+                  <div className="login__input-wrapper">
+                    <label className="login__label">Password</label>
+                    <Field
+                      className="login__input"
+                      name="password"
+                      type="password"
+                      placeholder="Your password"
+                    />
+                    <span className="login__icon">
+                      <i className="fas fa-eye"></i>
+                    </span>
+                  </div>
+                  <button
+                    className="login__button-login"
+                    type="submit"
+                    disabled={!formik.isValid}
+                  >
+                    Log in
+                  </button>
+                </Form>
+              </div>
             </div>
-            <div className="login__input-wrapper">
-              <label className="login__label">Password</label>
-              <input
-                className="login__input"
-                type="text"
-                placeholder="Password"
-              />
-              <span className="login__icon">
-                <i className="fas fa-eye"></i>
-              </span>
-            </div>
-            <button className="login__button-login">Log in</button>
-          </form>
-        </div>
-      </div>
-    </section>
+          </section>
+        );
+      }}
+    </Formik>
   );
 };
 
--- client/src/components/others/register/Register.jsx
+++ client/src/components/others/register/Register.jsx
@@ -1,95 +1,162 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 import "./Register.scss";
 
 import ButtonGoBackLoginRegister from "../buttonGoBackLoginRegister/ButtonGoBackLoginRegister";
 
 const Register = () => {
+  const initialValues = {
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Name is required"),
+    lastName: Yup.string().required("LastName is required"),
+    email: Yup.string().email("Invalid email").required("Email required"),
+    password: Yup.string().required("Password required"),
+    confirmPassword: Yup.string().when("password", {
+      is: (val) => (val && val.length > 0 ? true : false),
+      then: Yup.string().oneOf(
+        [Yup.ref("password")],
+        "Both passwords have to be the same"
+      ),
+    }),
+  });
+
+  const onSubmit = (values, submitProps) => {
+    console.log(values);
+    console.log(submitProps);
+    submitProps.resetForm();
+  };
+
+  const errorMsg = (props) => {
+    return <p className="register__error-msg">{props.children}</p>;
+  };
+
   return (
-    <section className="register">
-      <div className="register__wrapper">
-        <div className="register__left">
-          <span className="login__cover-left"></span>
-          <div className="register__left-redirects">
-            <h2 className="register__left-title">Sign up</h2>
-            <p className="register__left-subtitle">
-              Please fill in your details
-            </p>
-            <p className="register__left-login-text">
-              Already have an account?
-            </p>
-            <Link className="register__left-login-button" to="/login-admin">
-              Sign in
-            </Link>
-          </div>
-        </div>
-        <div className="register__right">
-          <ButtonGoBackLoginRegister />
-          <h2 className="register__right-title">Sign Up</h2>
-          <form className="register__form">
-            <div className="register__input-wrapper-name-surname">
-              <div className="register__wrapper-name">
-                <label className="register__label">First name</label>
-                <input
-                  className="register__input"
-                  type="text"
-                  placeholder="Name"
-                />
-                <span className="register__icon">
-                  <i className="far fa-user"></i>
-                </span>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {(formik) => {
+        console.log(formik);
+        return (
+          <section className="register">
+            <div className="register__wrapper">
+              <div className="register__left">
+                <span className="login__cover-left"></span>
+                <div className="register__left-redirects">
+                  <h2 className="register__left-title">Sign up</h2>
+                  <p className="register__left-subtitle">
+                    Please fill in your details
+                  </p>
+                  <p className="register__left-login-text">
+                    Already have an account?
+                  </p>
+                  <Link
+                    className="register__left-login-button"
+                    to="/login-admin"
+                  >
+                    Sign in
+                  </Link>
+                </div>
               </div>
-              <div className="register__wrapper-surname">
-                <label className="register__label">Last name</label>
-                <input
-                  className="register__input"
-                  type="text"
-                  placeholder="Surname"
-                />
-                <span className="register__icon">
-                  <i className="far fa-user"></i>
-                </span>
+              <div className="register__right">
+                <ButtonGoBackLoginRegister />
+                <h2 className="register__right-title">Sign Up</h2>
+                <Form className="register__form">
+                  <div className="register__input-wrapper-name-surname">
+                    <div className="register__wrapper-name">
+                      <ErrorMessage name="name" component={errorMsg} />
+                      <label className="register__label">First name</label>
+                      <Field
+                        className="register__input"
+                        name="name"
+                        type="text"
+                        placeholder="Your name"
+                      />
+                      <span className="register__icon">
+                        <i className="far fa-user"></i>
+                      </span>
+                    </div>
+                    <div className="register__wrapper-surname">
+                      <ErrorMessage name="lastName" component={errorMsg} />
+                      <label className="register__label">Last name</label>
+                      <Field
+                        className="register__input"
+                        name="lastName"
+                        type="text"
+                        placeholder="Your last name"
+                      />
+                      <span className="register__icon">
+                        <i className="far fa-user"></i>
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className="register__input-wrapper">
+                    <ErrorMessage name="email" component={errorMsg} />
+                    <label className="register__label">Email</label>
+                    <Field
+                      className="register__input"
+                      name="email"
+                      type="email"
+                      placeholder="Your email"
+                    />
+                    <span className="register__icon">
+                      <i className="far fa-envelope"></i>
+                    </span>
+                  </div>
+
+                  <div className="register__input-wrapper">
+                    <ErrorMessage name="password" component={errorMsg} />
+                    <label className="register__label">Password</label>
+                    <Field
+                      className="register__input"
+                      name="password"
+                      type="text"
+                      placeholder="Your password"
+                    />
+                    <span className="register__icon">
+                      <i className="fas fa-lock"></i>
+                    </span>
+                  </div>
+                  <div className="register__input-wrapper">
+                    <ErrorMessage name="confirmPassword" component={errorMsg} />
+                    <label className="register__label">Confirm Password</label>
+                    <Field
+                      className="register__input"
+                      name="confirmPassword"
+                      type="password"
+                      placeholder="Confirm password"
+                    />
+                    <span className="register__icon">
+                      <i className="fas fa-eye"></i>
+                    </span>
+                  </div>
+                  <button
+                    className="register__button-register"
+                    type="submit"
+                    disabled={!formik.isValid}
+                  >
+                    Sign up
+                  </button>
+                </Form>
               </div>
             </div>
-            <div className="register__input-wrapper">
-              <label className="register__label">Email</label>
-              <input
-                className="register__input"
-                type="email"
-                placeholder="Email"
-              />
-              <span className="register__icon">
-                <i className="far fa-envelope"></i>
-              </span>
-            </div>
-            <div className="register__input-wrapper">
-              <label className="register__label">Password</label>
-              <input
-                className="register__input"
-                type="text"
-                placeholder="Password"
-              />
-              <span className="register__icon">
-                <i className="fas fa-lock"></i>
-              </span>
-            </div>
-            <div className="register__input-wrapper">
-              <label className="register__label">Confirm Password</label>
-              <input
-                className="register__input"
-                type="password"
-                placeholder="Confirm Password"
-              />
-              <span className="register__icon">
-                <i className="fas fa-eye"></i>
-              </span>
-            </div>
-            <button className="register__button-register">Sign up</button>
-          </form>
-        </div>
-      </div>
-    </section>
+          </section>
+        );
+      }}
+    </Formik>
   );
 };
 
