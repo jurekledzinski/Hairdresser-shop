567cc5db32e636c0337a4f0b745d2e3f2ae34054
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-14 07:52:21 +0200

    Add if condition to check of lenght an array of state to prevent push sections every render

--- client/src/reduxStore/reducers/scrollReducer.js
+++ client/src/reduxStore/reducers/scrollReducer.js
@@ -3,9 +3,10 @@ import { ADD_SECTION } from "../actions/actionScroll";
 export const scrollReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_SECTION:
-      return [...state, action.payload];
+      if (state.length <= 5) {
+        return [...state, action.payload];
+      }
     default:
-      console.log(`Nie mamy akcji typu ${action.type}`);
       return state;
   }
 };
