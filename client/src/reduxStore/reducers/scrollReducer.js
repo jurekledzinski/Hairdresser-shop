a803bece972df88de249a468b66ec71b772578ef
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-18 12:05:06 +0200

    Add clear action to clear section when routes to have actual nodes of sections in array

--- client/src/reduxStore/reducers/scrollReducer.js
+++ client/src/reduxStore/reducers/scrollReducer.js
@@ -1,4 +1,4 @@
-import { ADD_SECTION } from "../actions/actionScroll";
+import { ADD_SECTION, CLEAR_SECTIONS } from "../actions/actionScroll";
 
 export const scrollReducer = (state = [], action) => {
   switch (action.type) {
@@ -6,6 +6,8 @@ export const scrollReducer = (state = [], action) => {
       if (state.length <= 5) {
         return [...state, action.payload];
       }
+    case CLEAR_SECTIONS:
+      return [];
     default:
       return state;
   }
