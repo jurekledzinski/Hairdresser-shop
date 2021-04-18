a560be849ec947fd231d5f3ca8255dd28b32aa6a
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-18 07:36:58 +0200

    Fetch and post testimonials from database

--- routes/testimonial.js
+++ routes/testimonial.js
@@ -3,14 +3,58 @@ const router = express.Router();
 
 const Testimonial = require("../models/testimonial.model");
 
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
 router.get("/", (req, res) => {
   Testimonial.find({})
     .then((response) => {
-      return res.status(200).json(response);
+      let shuffleArr = shuffle(response);
+      let ShuffleFour = shuffleArr.slice(0, 4);
+
+      return res.status(200).json(ShuffleFour);
     })
     .catch((err) => {
       console.log(err);
     });
+});
+
+router.post("/", (req, res) => {
+  const { name, imageUrl, opinion, rateStar } = req.body;
+
+  const info = {
+    alert: "",
+    success: "",
+  };
+
+  if (!name || !imageUrl || !opinion || !rateStar) {
+    info.alert = "Please fill in all fields";
+    return res.status(404).json(info);
+  }
+
+  if (!Boolean(info.alert)) {
+    const dataForm = {
+      name,
+      imageUrl,
+      opinion,
+      rateStar,
+    };
+
+    const newOpinion = new Testimonial(dataForm);
+
+    newOpinion
+      .save()
+      .then((response) => {
+        if (response) {
+          info.success = "Thank you for your opinion";
+          return res.status(200).json(info);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 });
 
 module.exports = router;
