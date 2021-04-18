78cfc8ccfbdf48ae407a313c3af04644225aeae6
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-18 12:03:24 +0200

    Modify custom hooks for header to have always current nodes to properly work scroll to sections and hightlight

--- client/src/components/header/customHeaderHooks/useHandleScrollToSection.js
+++ client/src/components/header/customHeaderHooks/useHandleScrollToSection.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
-const useHandleScrollToSection = (menuLinksRef) => {
+const useHandleScrollToSection = () => {
+  const [currentSections, setCurrentSections] = useState([]);
   const dataForScrollAnimation = useSelector((store) => store.scrollData);
+  const dataHeader = useSelector((store) => store.headerData);
 
   const handleScrollSectionAfterClickLink = (index) => {
-    if (menuLinksRef.current.length > 0) {
-      menuLinksRef.current.map((item, i) => {
+    if (dataHeader.length > 0) {
+      dataHeader.map((item, i) => {
         index === i
           ? item.childNodes[0].classList.add("header__menu-link--active")
           : item.childNodes[0].classList.remove("header__menu-link--active");
       });
     }
 
-    const clickedSection = dataForScrollAnimation[index];
+    const clickedSection = currentSections[index];
 
     window.scrollTo({
       top: isNaN((index * clickedSection.offsetTop) / index - 50)
@@ -23,6 +25,12 @@ const useHandleScrollToSection = (menuLinksRef) => {
       behavior: "smooth",
     });
   };
+
+  useEffect(() => {
+    if (dataForScrollAnimation.length > 0) {
+      setCurrentSections(dataForScrollAnimation);
+    }
+  }, [dataForScrollAnimation]);
 
   return { handleScrollSectionAfterClickLink };
 };
--- client/src/components/header/customHeaderHooks/useMoveScroll.js
+++ client/src/components/header/customHeaderHooks/useMoveScroll.js
@@ -1,33 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
-const useMoveScroll = (menuLinksRef, navigationHeader) => {
+const useMoveScroll = (navigationHeader) => {
+  const [currentSections, setCurrentSections] = useState([]);
   const dataForScrollAnimation = useSelector((store) => store.scrollData);
-
-  console.log(menuLinksRef, navigationHeader);
+  const dataHeader = useSelector((store) => store.headerData);
 
   const moveScroll = () => {
     if (
-      dataForScrollAnimation.length > 0 &&
-      Boolean(navigationHeader.current && menuLinksRef.current.length > 0)
+      currentSections.length > 0 &&
+      Boolean(navigationHeader.current && dataHeader.length > 0)
     ) {
-      dataForScrollAnimation.forEach((item, index) => {
+      currentSections.forEach((item, index) => {
         if (
           item.offsetTop - window.scrollY - 1 <
           -item.clientHeight + navigationHeader.current.clientHeight
         ) {
-          menuLinksRef.current[index].childNodes[0].classList.remove(
+          dataHeader[index].childNodes[0].classList.remove(
             "header__menu-link--active"
           );
         } else if (
           window.scrollY + navigationHeader.current.clientHeight + 1 >
           item.offsetTop
         ) {
-          menuLinksRef.current[index].childNodes[0].classList.add(
+          dataHeader[index].childNodes[0].classList.add(
             "header__menu-link--active"
           );
         } else {
-          menuLinksRef.current[index].childNodes[0].classList.remove(
+          dataHeader[index].childNodes[0].classList.remove(
             "header__menu-link--active"
           );
         }
@@ -38,7 +38,13 @@ const useMoveScroll = (menuLinksRef, navigationHeader) => {
     window.addEventListener("scroll", moveScroll);
 
     () => removeEventListener("scroll", moveScroll);
-  }, [dataForScrollAnimation, navigationHeader.current, menuLinksRef.current]);
+  }, [currentSections, dataHeader, navigationHeader.current]);
+
+  useEffect(() => {
+    if (dataForScrollAnimation.length > 0) {
+      setCurrentSections(dataForScrollAnimation);
+    }
+  }, [dataForScrollAnimation]);
 };
 
 export default useMoveScroll;
