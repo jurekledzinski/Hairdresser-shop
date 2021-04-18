f3d5e6b0cc16efc90240ecff1e818bec268adb78
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-18 07:36:00 +0200

    Custom hook for delete file from firebase when user leave form

---
+++ client/src/customHooks/useFirebaseDeleteFile.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+
+import { removeImageFile } from "../reduxStore/actions/actionFile";
+
+import { projectStorage } from "../firebase/config";
+
+const useFirebseDeleteFile = (imgLink) => {
+  const dispatch = useDispatch();
+
+  const history = useHistory();
+
+  useEffect(() => {
+    history.listen(() => {
+      const fireBaseUrlStorage = "firebasestorage";
+      if (
+        (history.location.pathname !== "/register-admin" &&
+          Boolean(imgLink.current)) ||
+        (history.location.pathname !== "/" && Boolean(imgLink.current))
+      ) {
+        if (
+          Boolean(imgLink.current) &&
+          imgLink.current.indexOf(fireBaseUrlStorage) !== -1
+        ) {
+          const image = projectStorage.refFromURL(imgLink.current);
+          imgLink.current = null;
+          dispatch(removeImageFile(null, null));
+          image
+            .delete()
+            .then((responese) => responese)
+            .catch((err) => {
+              console.warn(err);
+            });
+        }
+      }
+    });
+  }, [imgLink]);
+};
+
+export default useFirebseDeleteFile;
