5f370652eef96d2bdb260aa410cb3754a9e08cbf
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-18 07:42:06 +0200

    Add reducer to combine in rootreducer

--- client/src/reduxStore/reducers/rootReducer.js
+++ client/src/reduxStore/reducers/rootReducer.js
@@ -1,8 +1,18 @@
 import { combineReducers } from "redux";
-import { scrollReducer } from "./scrollReducer";
+import { alertMessagesReducer } from "./alertMessagesReducer";
+import { usersReducer } from "./actionFetchAdminReducer";
+import { fileReducer } from "./fileReducer";
+import { headerReducer } from "./headerReducer";
+import { opinionsReducer } from "./opinionsReducer";
 import { resizeWindowReducer } from "./resizeWindowReducer";
+import { scrollReducer } from "./scrollReducer";
 
 export const rootReducer = combineReducers({
-  scrollData: scrollReducer,
+  alertData: alertMessagesReducer,
+  fileDate: fileReducer,
+  headerData: headerReducer,
   resizeData: resizeWindowReducer,
+  scrollData: scrollReducer,
+  testimonialData: opinionsReducer,
+  userData: usersReducer,
 });
