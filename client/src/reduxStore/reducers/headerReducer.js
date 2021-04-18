1b302e430d7cf5ffb81dd47f1d6da9adbe61657d
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-18 12:06:16 +0200

    Create remove links action to have current nodes of links in array when change routes

--- client/src/reduxStore/reducers/headerReducer.js
+++ client/src/reduxStore/reducers/headerReducer.js
@@ -1,12 +1,13 @@
-import { ADD_MENU_LINKS } from "../actions/actionsHeader";
+import { ADD_MENU_LINKS, REMOVE_MENU_LINKS } from "../actions/actionsHeader";
 
 export const headerReducer = (state = [], action) => {
-  console.log(action.type);
   switch (action.type) {
     case ADD_MENU_LINKS:
       if (state.length <= 5) {
         return [...state, action.payload];
       }
+    case REMOVE_MENU_LINKS:
+      return [];
     default:
       return state;
   }
