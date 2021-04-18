1c512cbb57f217c6ea71d17a03becee2d90af2e6
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-18 07:40:32 +0200

    Create configuration file for passport.js

---
+++ configs/passport.js
@@ -0,0 +1,46 @@
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+
+const RegisterAdmin = require("../models/registerAdmin.model.js");
+
+module.exports = function (passport) {
+  passport.use(
+    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+      RegisterAdmin.findOne({ email: email }, (err, user) => {
+        if (err) return done(err);
+
+        if (!user) {
+          return done(null, false);
+        }
+
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
+        });
+      });
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    RegisterAdmin.findById(id, function (err, user) {
+      console.log(user, "passport");
+      const adminUser = {
+        user: user.name,
+        email: user.email,
+        imageUrl: user.imageUrl,
+        userId: user._id,
+        role: user.role,
+      };
+      done(err, adminUser);
+    });
+  });
+};
