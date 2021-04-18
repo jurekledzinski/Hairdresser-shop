a623e0555145edde547c8c37361f2aae067671e1
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-18 07:42:44 +0200

    remove console.log from resizeWindowReducer

--- client/src/reduxStore/reducers/resizeWindowReducer.js
+++ client/src/reduxStore/reducers/resizeWindowReducer.js
@@ -4,12 +4,10 @@ export const resizeWindowReducer = (
   state = { size: window.innerWidth },
   action
 ) => {
-  console.log(action);
   switch (action.type) {
     case GET_SIZE_WINDOW:
       return action.payload;
     default:
-      console.log(`Nie mamy akcji typu ${action.type}`);
       return state;
   }
 };
