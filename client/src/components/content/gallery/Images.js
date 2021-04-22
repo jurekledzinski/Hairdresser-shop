8397de3ef2cbbaf5a76d87de68d017ff1d83c9d0
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-22 22:39:30 +0200

    Remove uneccessary property in dummy data images

--- client/src/components/content/gallery/Images.js
+++ client/src/components/content/gallery/Images.js
@@ -4,42 +4,36 @@ export const imagesMen = [
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Men-haircut-1.jpg?alt=media&token=e421f452-13ca-49d1-8fab-7463dd1f577a",
     alt: "Man haircut",
-    numImg: 1,
   },
   {
     id: 1,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Men-haircut-2.jpg?alt=media&token=77eb3053-baab-4f2f-befd-308a7576ab68",
     alt: "Picture Thgusstavo Santana from Pexels",
-    numImg: 2,
   },
   {
     id: 2,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Men-haircut-3.jpg?alt=media&token=5e1e13fe-926c-4046-afe2-856c228c24eb",
     alt: "Picture Thgusstavo Santana from Pexels",
-    numImg: 3,
   },
   {
     id: 3,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Men-haircut-4.jpg?alt=media&token=bb260fac-76c2-4188-9e78-950420d07c1e",
     alt: "Picture cottonbro from Pexels",
-    numImg: 4,
   },
   {
     id: 4,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Men-haircut-5.jpg?alt=media&token=3dc5b346-b4e3-4476-a072-d5a2d7e9c5f9",
     alt: "Picture Felipe Vallin from Pexels",
-    numImg: 5,
   },
   {
     id: 5,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Men-haircut-6.jpg?alt=media&token=96edcaa7-620a-4f84-9c17-fa58b1ff4017",
     alt: "Man haircut",
-    numImg: 6,
   },
 ];
 
@@ -49,42 +43,36 @@ export const imagesWomen = [
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-haircut-1.jpg?alt=media&token=bfed66d5-e6ba-4b27-996d-4ec68b12a636",
     alt: "Picture cottonbro from Pexels",
-    numImg: 1,
   },
   {
     id: 1,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-haircut-2.jpg?alt=media&token=4ed27aa9-3ad9-4282-8b48-de2c19da788b",
     alt: "Picture Tatiana Twinslol from Pexels",
-    numImg: 2,
   },
   {
     id: 2,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-haircut-3.jpg?alt=media&token=352a3353-6184-4e3b-9a21-fb044825f405",
     alt: "Picture Element5 Digital from Pexels",
-    numImg: 3,
   },
   {
     id: 3,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-haircut-4.jpg?alt=media&token=6c1c6037-6f0e-4b36-8bbe-ab04c09fe98e",
     alt: "Picture Engin Akyurt from Pexels",
-    numImg: 4,
   },
   {
     id: 4,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-haircut-5.jpg?alt=media&token=675155ad-5e8d-4539-9c24-50aba982406b",
     alt: "Picture Engin Akyurt from Pexels",
-    numImg: 5,
   },
   {
     id: 5,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-haircut-6.jpg?alt=media&token=551e0cdb-4d57-4a57-b031-8c36f84e85cc",
     alt: "Woman hairuct",
-    numImg: 6,
   },
 ];
 
@@ -94,42 +82,36 @@ export const imageChildren = [
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Children-haircut-1.jpg?alt=media&token=705be2ad-d380-412d-bfde-47a103a5db05",
     alt: "Child haircut",
-    numImg: 1,
   },
   {
     id: 1,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Children-haircut-2.jpg?alt=media&token=b7c56c04-035f-4008-b6bf-abd87610fb72",
     alt: "Child haircut",
-    numImg: 2,
   },
   {
     id: 2,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Children-haircut-3.jpg?alt=media&token=d71d29fc-55ea-45fb-b3d5-b2df0d060052",
     alt: "Child haircut",
-    numImg: 3,
   },
   {
     id: 3,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Children-haircut-4.jpg?alt=media&token=0ddaa232-116b-473a-881b-3aba2fe1b445",
     alt: "Child haircut",
-    numImg: 4,
   },
   {
     id: 4,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Children-haircut-5.jpg?alt=media&token=8cb18301-1bf3-4d1a-afd1-4d850cdde582",
     alt: "Child haircut",
-    numImg: 5,
   },
   {
     id: 5,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Children-haircut-6.jpg?alt=media&token=9f7cba20-0d9f-41c0-b3ce-35cfe42b0b6c",
     alt: "Child haircut",
-    numImg: 6,
   },
 ];
 
@@ -139,42 +121,36 @@ export const imagesWeddings = [
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Wedding-haircut-1.jpg?alt=media&token=2fd2f147-9e20-4e6e-b72a-8dcbe387a2e5",
     alt: "Picture Engin Akyur from Pexels",
-    numImg: 1,
   },
   {
     id: 1,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Wedding-haircut-2.jpg?alt=media&token=f869dd73-affe-4d3c-9619-2b71b28dc32a",
     alt: "Picture Maria Lindsey Content Creator from Pexels",
-    numImg: 2,
   },
   {
     id: 2,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Wedding-haircut-3.jpg?alt=media&token=82f15d8a-b99a-4c66-a421-4beaced75e63",
     alt: "Wedding haircut",
-    numImg: 3,
   },
   {
     id: 3,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Wedding-haircut-4.jpg?alt=media&token=229b0a6c-0691-4fe9-bd22-6924f763a67f",
     alt: "Wedding haircut",
-    numImg: 4,
   },
   {
     id: 4,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Wedding-haircut-5.jpg?alt=media&token=63e8a2b7-5777-4ce9-a4b9-ca90221a4d1f",
     alt: "Wedding haircut",
-    numImg: 5,
   },
   {
     id: 5,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Wedding-haircut-6.jpg?alt=media&token=b41d68e0-856e-4f06-8654-39f4515dae94",
     alt: "Wedding haircut",
-    numImg: 6,
   },
 ];
 
@@ -184,42 +160,36 @@ export const imagesOthers = [
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Others-haircut-1.jpg?alt=media&token=39cf4943-da18-42c5-9957-076064be5eda",
     alt: "Picture Luis Quintero from Pexels",
-    numImg: 1,
   },
   {
     id: 1,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Others-haircut-2.jpg?alt=media&token=350ef8f3-63d7-4a43-b300-3a05f268b3b0",
     alt: "Picture Dmitry Zvolskiy from Pexels",
-    numImg: 2,
   },
   {
     id: 2,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Others-haircut-3.jpg?alt=media&token=e04a2d70-937a-4b7b-9c4c-f8df23feb477",
     alt: "Picture Maksim Goncharenok from Pexels",
-    numImg: 3,
   },
   {
     id: 3,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Others-haircut-4.jpg?alt=media&token=1be978d6-1ead-4a03-811d-6d97c51987a8",
     alt: "Picture Jan Kopřiva from Pexels",
-    numImg: 4,
   },
   {
     id: 4,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Others-haircut-5.jpg?alt=media&token=2f6afcbe-b19b-49b6-89ad-9b7a808147da",
     alt: "Picture Thgusstavo Santana from Pexels",
-    numImg: 5,
   },
   {
     id: 5,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Others-haircut-6.jpg?alt=media&token=81dce47a-ca6d-4d3f-88d7-b6bd3ce207df",
     alt: "Man haircut",
-    numImg: 6,
   },
 ];
 
@@ -231,42 +201,36 @@ export const imgSliderMen = [
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Men-slider-1.jpg?alt=media&token=95944aa1-15ce-4403-983a-3e893032e206",
     alt: "Man haircut",
-    numImg: 1,
   },
   {
     id: 1,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Men-slider-2.jpg?alt=media&token=78a4f515-3005-4e93-baec-aa451e792f09",
     alt: "Picture Thgusstavo Santana from Pexels",
-    numImg: 2,
   },
   {
     id: 2,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Men-slider-3.jpg?alt=media&token=bac6ffab-961e-4a26-b20e-db02e57de11d",
     alt: "Picture Thgusstavo Santana from Pexels",
-    numImg: 3,
   },
   {
     id: 3,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Men-slider-4.jpg?alt=media&token=bdb360f6-1d71-48a2-ae65-223a90474975",
     alt: "Picture cottonbro from Pexels",
-    numImg: 4,
   },
   {
     id: 4,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Men-slider-5.jpg?alt=media&token=549572dc-b702-4d85-b13a-c6cb0730958f",
     alt: "Picture Felipe Vallin from Pexels",
-    numImg: 5,
   },
   {
     id: 5,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Men-slider-6.jpg?alt=media&token=7c45d9d9-ebd5-4577-b895-db9bec127241",
     alt: "Man haircut",
-    numImg: 6,
   },
 ];
 
@@ -276,42 +240,36 @@ export const imgSliderWomen = [
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-slider-1.jpg?alt=media&token=7848810e-e127-4447-b756-5af21d7cdb70",
     alt: "Picture cottonbro from Pexels",
-    numImg: 1,
   },
   {
     id: 1,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-slider-2.jpg?alt=media&token=7b5141dc-8b67-4864-b2f7-6bc51f50eff1",
     alt: "Picture Tatiana Twinslol from Pexels",
-    numImg: 2,
   },
   {
     id: 2,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-slider-3.jpg?alt=media&token=a69fc524-bef1-483d-b7bf-9407d884b31c",
     alt: "Picture Element5 Digital from Pexels",
-    numImg: 3,
   },
   {
     id: 3,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-slider-4.jpg?alt=media&token=40d36270-34ec-4ec5-9f9b-32597bfa6a80",
     alt: "Picture Engin Akyurt from Pexels",
-    numImg: 4,
   },
   {
     id: 4,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-slider-5.jpg?alt=media&token=93cba570-4ffe-4855-b7c1-1ef5f8e0b9ce",
     alt: "Picture Engin Akyurt from Pexels",
-    numImg: 5,
   },
   {
     id: 5,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-slider-6.jpg?alt=media&token=63fe774b-7563-415e-826e-01f4f66c461b",
     alt: "Woman hairuct",
-    numImg: 6,
   },
 ];
 
@@ -321,42 +279,36 @@ export const imgSliderChildren = [
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Children-slider-1.jpg?alt=media&token=d6e74ece-c12d-44fd-8123-aeade41db8c4",
     alt: "Child haircut",
-    numImg: 1,
   },
   {
     id: 1,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Children-slider-2.jpg?alt=media&token=d21d1d50-6c68-4693-8fa7-d267cf1ed0ae",
     alt: "Child haircut",
-    numImg: 2,
   },
   {
     id: 2,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Children-slider-3.jpg?alt=media&token=ea924cdb-9701-4551-aa2f-b4ebce3ef037",
     alt: "Child haircut",
-    numImg: 3,
   },
   {
     id: 3,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Children-slider-4.jpg?alt=media&token=babd4ed3-1ecf-4382-a0af-35eb5881b9be",
     alt: "Child haircut",
-    numImg: 4,
   },
   {
     id: 4,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Children-slider-5.jpg?alt=media&token=8d8066bc-ffee-4d2b-a3db-0f5cd755aaa3",
     alt: "Child haircut",
-    numImg: 5,
   },
   {
     id: 5,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Children-slider-6.jpg?alt=media&token=baef1749-ed08-4fd4-8928-1a3657c5e2fb",
     alt: "Child haircut",
-    numImg: 6,
   },
 ];
 
@@ -366,42 +318,36 @@ export const imgSliderWeddings = [
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Wedding-slider-1.jpg?alt=media&token=1e94a49e-1c61-4251-8834-b5ae2063d3a3",
     alt: "Picture Engin Akyur from Pexels",
-    numImg: 1,
   },
   {
     id: 1,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Wedding-slider-2.jpg?alt=media&token=9218a5bd-70ec-40ee-8587-ffde896cfde7",
     alt: "Picture Maria Lindsey Content Creator from Pexels",
-    numImg: 2,
   },
   {
     id: 2,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Women-slider-3.jpg?alt=media&token=a69fc524-bef1-483d-b7bf-9407d884b31c",
     alt: "Wedding haircut",
-    numImg: 3,
   },
   {
     id: 3,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Wedding-slider-4.jpg?alt=media&token=d1158e5b-124e-47f2-a960-5a466aeaf36a",
     alt: "Wedding haircut",
-    numImg: 4,
   },
   {
     id: 4,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Wedding-slider-5.jpg?alt=media&token=c71c8311-a60c-4133-925e-4cdc1a4572bd",
     alt: "Wedding haircut",
-    numImg: 5,
   },
   {
     id: 5,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Wedding-slider-6.jpg?alt=media&token=f28b7c9a-6cd6-4b6a-b856-376acd85bc6b",
     alt: "Wedding haircut",
-    numImg: 6,
   },
 ];
 
@@ -411,41 +357,35 @@ export const imgSliderOthers = [
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Others-slider-1.jpg?alt=media&token=4713f0a7-0188-41c5-9d91-b32829470c83",
     alt: "Picture Luis Quintero from Pexels",
-    numImg: 1,
   },
   {
     id: 1,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Others-slider-2.jpg?alt=media&token=2e1eaee9-5fd1-4f86-8b87-f2e71c484071",
     alt: "Picture Dmitry Zvolskiy from Pexels",
-    numImg: 2,
   },
   {
     id: 2,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Others-slider-3.jpg?alt=media&token=f7943956-bd59-4dc2-81d4-44c291a0d3e8",
     alt: "Picture Maksim Goncharenok from Pexels",
-    numImg: 3,
   },
   {
     id: 3,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Others-slider-4.jpg?alt=media&token=c9e0df58-dd28-4ac9-b287-4fda36942ce6",
     alt: "Picture Jan Kopřiva from Pexels",
-    numImg: 4,
   },
   {
     id: 4,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Others-slider-5.jpg?alt=media&token=9123a586-ca13-4414-80b5-c29bffd533c9",
     alt: "Picture Thgusstavo Santana from Pexels",
-    numImg: 5,
   },
   {
     id: 5,
     imagePath:
       "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Others-slider-6.jpg?alt=media&token=48eef033-6b43-4c95-ae08-d66550ce2f2e",
     alt: "Man haircut",
-    numImg: 6,
   },
 ];
