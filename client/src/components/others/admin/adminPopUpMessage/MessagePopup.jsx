e0f0bc21d28d3d912008677140357ee6e84b955d
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-22 22:51:14 +0200

    Create MessagePopUp component to display on admin page

---
+++ client/src/components/others/admin/adminPopUpMessage/MessagePopup.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import "./MessagePopup.scss";
+
+import Modal from "../../modal/Modal";
+
+const MessagePopup = ({
+  isOpenModal,
+  handleRemoveItem,
+  handleNotRemoveItem,
+}) => {
+  return (
+    <Modal isOpen={isOpenModal}>
+      <div
+        className={
+          isOpenModal ? "message-popup message-popup--active" : "message-popup"
+        }
+      >
+        <h4 className="message-popup__title">
+          Are you sure you want remove this email?
+        </h4>
+        <div className="message-popup__wrapper-btns">
+          <button
+            className="message-popup__button-yes"
+            onClick={handleRemoveItem}
+          >
+            Yes
+          </button>
+          <button
+            className="message-popup__button-no"
+            onClick={handleNotRemoveItem}
+          >
+            No
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default MessagePopup;
