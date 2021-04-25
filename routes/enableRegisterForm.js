aa515c17e7f5e50716c79d70aec07914c7ca0af0
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-25 23:21:00 +0200

    Add routers with http method for enable register form

---
+++ routes/enableRegisterForm.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+
+const EnableRegisterForm = require("../models/enableRegisterForm.model");
+
+const { ErrorHandler } = require("../errors/error");
+
+router.get("/", (req, res) => {
+  EnableRegisterForm.find({})
+    .then((response) => {
+      return res.status(200).json(response);
+    })
+    .catch((err) => {
+      next(new ErrorHandler(500, "Internal server error", err.message));
+    });
+});
+
+router.put("/", (req, res) => {
+  const { enableRegisterForm } = req.body;
+
+  let info = {
+    alert: "",
+    success: "",
+  };
+
+  EnableRegisterForm.findOne()
+    .then((response) => {
+      if (response) {
+        response.enableRegisterForm = enableRegisterForm;
+
+        info.success = "Registration enabled";
+
+        response.save().then((response) => {
+          return res.status(200).json(info);
+        });
+      } else {
+        let enableEndpoint = {
+          enableRegisterForm,
+        };
+
+        const newEnable = new EnableRegisterForm(enableEndpoint);
+
+        newEnable
+          .save()
+          .then((response) => {
+            if (response) {
+              res.status(200).end();
+            }
+          })
+          .catch((err) => {
+            next(new ErrorHandler(500, "Internal server error", err.message));
+          });
+      }
+    })
+    .catch((err) => {
+      next(new ErrorHandler(500, "Internal server error", err.message));
+    });
+});
+
+module.exports = router;
