5d0b21c447dd1a14fe55eb779930a378b93121ef
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-04-29 12:26:11 +0200

    Add loader to background image in booking

--- client/src/components/others/booking/BookingMainPage.jsx
+++ client/src/components/others/booking/BookingMainPage.jsx
@@ -7,13 +7,25 @@ import BookingCancelForm from "./BookingCancelForm";
 import BookingTermsConditions from "./BookingTermsConditions";
 import BookingGoBackButton from "./BookingGoBackButton";
 
+import useLoadBgImg from "../../../customHooks/useLoadBgImg";
+import CircleSpinner from "../spinner/CircleSpinner";
+
 const BookingMainPage = () => {
+  const bookUrl =
+    "https://firebasestorage.googleapis.com/v0/b/hairdress-shop.appspot.com/o/Background-booking.jpeg?alt=media&token=043b3596-878c-49ff-a10c-002b66da3570";
+  const isLoad = useLoadBgImg(bookUrl);
   return (
     <section className="booking">
       <div className="booking__center-wrapper">
         <BookingGoBackButton />
         <div className="booking__wrapper">
-          <div className="booking__left"></div>
+          <div className="booking__left">
+            {!isLoad && (
+              <div className="booking__overlay">
+                <CircleSpinner />
+              </div>
+            )}
+          </div>
           <div className="booking__right">
             <div className="booking__inside-wrapper">
               <h2 className="booking__title">Book seat now</h2>
