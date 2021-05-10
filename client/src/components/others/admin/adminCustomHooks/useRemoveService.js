e1970d15c1304298c93904e20cad84fe8cbb88ce
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-05-10 22:39:55 +0200

    Add remove image from firebase when remove service in admin panel

--- client/src/components/others/admin/adminCustomHooks/useRemoveService.js
+++ client/src/components/others/admin/adminCustomHooks/useRemoveService.js
@@ -8,12 +8,16 @@ import {
 
 import { deleteAdminService } from "../../../../utils/sessions";
 
+import useDeleteFileFirebase from "../../../../customHooks/useDeleteFileFirebase";
+
 const useRemoveService = (
   currentServices,
   idService,
+  imgUrl,
   setCurrentServices,
   setIsOpenModal
 ) => {
+  const { deleteImgFirebase } = useDeleteFileFirebase();
   const dispatch = useDispatch();
 
   const deepCopyCurrentServices = () => {
@@ -30,6 +34,7 @@ const useRemoveService = (
     const { data, status } = await deleteAdminService(idService);
 
     if (status === 200) {
+      deleteImgFirebase(imgUrl.current);
       dispatch(addServerSuccessMessage(data.success, "removeAtTableAdmin"));
       let newCopy = deepCopyCurrentServices();
       const updatedService = newCopy.filter((item) => item._id !== idService);
