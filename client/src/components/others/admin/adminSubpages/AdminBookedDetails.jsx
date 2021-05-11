3c2158a02460a1e86921aaada22207edf5db8123
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-05-11 21:30:49 +0200

    Create subpages to display details booked and canceled orders

---
+++ client/src/components/others/admin/adminSubpages/AdminBookedDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./AdminBookedDetails.scss";
+
+const AdminBookedDetails = () => {
+  return <div>Details Booked</div>;
+};
+
+export default AdminBookedDetails;
---
+++ client/src/components/others/admin/adminSubpages/AdminCanceledDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./AdminCanceledDetails.scss";
+
+const AdminCanceledDetails = () => {
+  return <div>Details canceled booking</div>;
+};
+
+export default AdminCanceledDetails;
