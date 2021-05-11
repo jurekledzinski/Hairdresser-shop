924ff6e7db65b35305f8d3d5f0f45d5617d168fa
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-05-11 21:27:08 +0200

    Add route to component which show page when user cancel order

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import BookingMainPage from "./components/others/booking/BookingMainPage";
 import BookingDetails from "./components/others/booking/BookingDetails";
 import BookingSuccess from "./components/others/booking/BookingSuccess";
 import BookingCancel from "./components/others/booking/BookingCancel";
+import BookingCancelByCode from "./components/others/booking/BookingCancelByCode";
 import TeamDetails from "./components/content/team/TeamDetails";
 import BookingTermPolicyDetails from "./components/others/booking/BookingTermPolicyDetails";
 import AdminDashboard from "./components/others/admin/adminDashboard/AdminDashboard";
@@ -30,7 +31,13 @@ const App = () => {
               <Route exact path="/booking" component={BookingMainPage} />
               <Route path="/booking/details/:id" component={BookingDetails} />
               <Route path="/booking/success/:id" component={BookingSuccess} />
+
               <Route path="/booking/cancel/:id" component={BookingCancel} />
+              <Route
+                path="/booking/cancel-code/:id"
+                component={BookingCancelByCode}
+              />
+
               <Route path="/team-details" component={TeamDetails} />
               <Route path="/term-policy" component={BookingTermPolicyDetails} />
               <Route path="/admin" component={AdminDashboard} />
