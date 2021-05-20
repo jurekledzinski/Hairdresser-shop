8ee7d10f3a0d41e31f016c3f027fb2e29be59388
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-05-20 12:53:49 +0200

    Create component DotLoader to show when lazy loading pages fire

---
+++ client/src/components/others/dotLoader/DotLoader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./DotLoader.scss";
+
+const DotLoader = () => {
+  return (
+    <div className="dotsAnimation">
+      <span className="dotsAnimation__dot"></span>
+      <span className="dotsAnimation__dot"></span>
+      <span className="dotsAnimation__dot"></span>
+    </div>
+  );
+};
+
+export default DotLoader;
