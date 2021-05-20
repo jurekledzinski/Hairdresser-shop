dbe4f35f0e52b935073cfcd75878a07379a04026
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-05-20 12:41:04 +0200

    Add lazy loading and style text for log out admin

--- client/src/components/others/admin/AdminContent.jsx
+++ client/src/components/others/admin/AdminContent.jsx
@@ -1,50 +1,112 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
+import { Switch } from "react-router-dom";
+
+import { Role } from "../../../helpers/roles";
 
 import "./AdminContent.scss";
 
-import AdminDashboardPanel from "./adminSubpages/AdminDashboardPanel";
-import AdminBooked from "./adminSubpages/AdminBooked";
-import AdminBookedDetails from "./adminSubpages/AdminBookedDetails";
-import AdminCanceled from "./adminSubpages/AdminCanceled";
-import AdminCanceledDetails from "./adminSubpages/AdminCanceledDetails";
-import AdminEmails from "./adminSubpages/AdminEmails";
-import AdminGallery from "./adminSubpages/AdminGallery";
-import AdminAppointments from "./adminSubpages/AdminAppointments";
-import AdminAppointmentDetails from "./adminSubpages/AdminAppointmentDetails";
-import AdminShop from "./adminSubpages/AdminShop";
-import AdminOpinions from "./adminSubpages/AdminOpinions";
-import AdminProfile from "./adminSubpages/AdminProfile";
-import AdminService from "./adminSubpages/AdminService";
+const AdminDashboardPanel = lazy(() =>
+  import("./adminSubpages/AdminDashboardPanel")
+);
+const AdminBooked = lazy(() => import("./adminSubpages/AdminBooked"));
+const AdminBookedDetails = lazy(() =>
+  import("./adminSubpages/AdminBookedDetails")
+);
+const AdminCanceled = lazy(() => import("./adminSubpages/AdminCanceled"));
+const AdminCanceledDetails = lazy(() =>
+  import("./adminSubpages/AdminCanceledDetails")
+);
+const AdminEmails = lazy(() => import("./adminSubpages/AdminEmails"));
+const AdminGallery = lazy(() => import("./adminSubpages/AdminGallery"));
+const AdminAppointments = lazy(() =>
+  import("./adminSubpages/AdminAppointments")
+);
+const AdminAppointmentDetails = lazy(() =>
+  import("./adminSubpages/AdminAppointmentDetails")
+);
+const AdminShop = lazy(() => import("./adminSubpages/AdminShop"));
+const AdminOpinions = lazy(() => import("./adminSubpages/AdminOpinions"));
+const AdminProfile = lazy(() => import("./adminSubpages/AdminProfile"));
+const AdminService = lazy(() => import("./adminSubpages/AdminService"));
+
+import { ProtectAdmin } from "../../../protectRoutes/ProtectAdminRoutes";
+
+import DotLoader from "../dotLoader/DotLoader";
 
 const AdminContent = ({ isLogOutMsg }) => {
   return (
     <div className="admin-content">
-      {isLogOutMsg && <p className="admin__logout">{isLogOutMsg}</p>}
-      <Switch>
-        <Route exact path="/admin" component={AdminDashboardPanel} />
-        <Route path="/admin/booked" component={AdminBooked} />
-        <Route
-          path="/admin/details-booked-order/:id"
-          component={AdminBookedDetails}
-        />
-        <Route path="/admin/canceled" component={AdminCanceled} />
-        <Route
-          path="/admin/details-canceled-order/:id"
-          component={AdminCanceledDetails}
-        />
-        <Route path="/admin/emails" component={AdminEmails} />
-        <Route path="/admin/gallery" component={AdminGallery} />
-        <Route path="/admin/apponitments" component={AdminAppointments} />
-        <Route
-          path="/admin/details-appointment/:id"
-          component={AdminAppointmentDetails}
-        />
-        <Route path="/admin/shop" component={AdminShop} />
-        <Route path="/admin/opinions" component={AdminOpinions} />
-        <Route path="/admin/profile" component={AdminProfile} />
-        <Route path="/admin/service" component={AdminService} />
-      </Switch>
+      <Suspense fallback={<DotLoader />}>
+        {isLogOutMsg && <p className="admin-content__logout">{isLogOutMsg}</p>}
+        <Switch>
+          <ProtectAdmin
+            exact
+            path="/admin"
+            component={AdminDashboardPanel}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+          <ProtectAdmin
+            path="/admin/booked"
+            component={AdminBooked}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+          <ProtectAdmin
+            path="/admin/details-booked-order/:id"
+            component={AdminBookedDetails}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+          <ProtectAdmin
+            path="/admin/canceled"
+            component={AdminCanceled}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+          <ProtectAdmin
+            path="/admin/details-canceled-order/:id"
+            component={AdminCanceledDetails}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+          <ProtectAdmin
+            path="/admin/emails"
+            component={AdminEmails}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+          <ProtectAdmin
+            path="/admin/gallery"
+            component={AdminGallery}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+          <ProtectAdmin
+            path="/admin/apponitments"
+            component={AdminAppointments}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+          <ProtectAdmin
+            path="/admin/details-appointment/:id"
+            component={AdminAppointmentDetails}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+          <ProtectAdmin
+            path="/admin/shop"
+            component={AdminShop}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+          <ProtectAdmin
+            path="/admin/opinions"
+            component={AdminOpinions}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+          <ProtectAdmin
+            path="/admin/profile"
+            component={AdminProfile}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+          <ProtectAdmin
+            path="/admin/service"
+            component={AdminService}
+            roles={[Role.Admin, Role.SuperAdmin]}
+          />
+        </Switch>
+      </Suspense>
     </div>
   );
 };
