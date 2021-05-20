6b467afdb7cd2051601ee2b1374fcafcd53a746a
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-05-20 13:05:06 +0200

    Add reducers to root reducer

--- client/src/reduxStore/reducers/rootReducer.js
+++ client/src/reduxStore/reducers/rootReducer.js
@@ -2,9 +2,14 @@ import { combineReducers } from "redux";
 import { adminDataReducer } from "./adminDataReducer";
 import { alertMessagesReducer } from "./alertMessagesReducer";
 import { bookedOrdersReducer } from "./bookedOrdersReducer";
+import { bookingsMadeAtShopReducer } from "./bookingsMadeAtShopReducer";
+import { bookingsMadeWebsiteReducer } from "./bookingsMadeAtWebsiteReducer";
 import { canceledOrdersReducer } from "./canceledOrdersReducer";
+import { emailDataReducer } from "./emailsDataReducer";
 import { orderDetailsReducer } from "./orderDetailsReducer";
 import { usersReducer } from "./actionFetchAdminReducer";
+import { fetchAmountBookingsPerMonthInShopReducer } from "./fetchAmountBookingsPerMonthInShopReducer";
+import { fetchAmountBookingsPerMonthWebsiteReducer } from "./fetchAmountBookingsPerMonthInWebsiteReducer";
 import { fetchAllAdminsReducer } from "./fetchAllAdminsReducer";
 import { fetchAllBookedOrdersReducer } from "./fetchBookedOrdersReducer";
 import { fetchAllCancledOrdersReducer } from "./fetchCanceledOrdersReducer";
@@ -15,11 +20,17 @@ import { fetchEmailsReducer } from "./fetchEmailsReducer";
 import { fetchImagesGalleryReducer } from "./fetchGalleryImagesReducer";
 import { fetchOpinionsReducer } from "./fetchOpinionsReducer";
 import { fetchPermissionToRegisterReducer } from "./fetchPermissionRegisterReducer";
+import { fetchPaymentsShopReducer } from "./fetchPaymentsMonthShopReducer";
+import { fetchPaymentsWebsiteReducer } from "./fetchPaymentsMonthWebsiteReducer";
 import { fetchServicesReducer } from "./fetchServicesReducer";
 import { fileReducer } from "./fileReducer";
+import { firebaseUrlReducer } from "./firebseUrlReducer";
+import { galleryImagesReducer } from "./galleryImagesReducer";
 import { excludedTimesReducer } from "./excludedTimesReducer";
 import { headerReducer } from "./headerReducer";
-import { opinionsReducer } from "./opinionsReducer";
+import { opinionsDataReducer } from "./opinionsDataReducer";
+import { paymentsShopReducer } from "./paymentsMonthShopReducer";
+import { paymentsWebsiteReducer } from "./paymentsMonthWebsiteReducer";
 import { fetchOpenShopReducer } from "./fetchOpenShopReducer";
 import { resizeWindowReducer } from "./resizeWindowReducer";
 import { scrollReducer } from "./scrollReducer";
@@ -28,29 +39,40 @@ import { singleExcludedTimeReducer } from "./singleExcludedTimeReducer.";
 
 export const rootReducer = combineReducers({
   adminsData: fetchAllAdminsReducer,
+  amountBookingsDataShop: fetchAmountBookingsPerMonthInShopReducer,
+  amountBookingsDataWebsite: fetchAmountBookingsPerMonthWebsiteReducer,
   allBookedOrdersData: fetchAllBookedOrdersReducer,
   allCanceledOrdersData: fetchAllCancledOrdersReducer,
   alertData: alertMessagesReducer,
   allServicesData: fetchAllServicesReducer,
+  bookingsMadeAtShopData: bookingsMadeAtShopReducer,
+  bookingsMadeAtWebsiteData: bookingsMadeWebsiteReducer,
   bookedOrdersData: bookedOrdersReducer,
   bookingUserData: fetchBookingUserReducer,
   canceledOrderData: canceledOrdersReducer,
   orderDetailsData: orderDetailsReducer,
   emailsData: fetchEmailsReducer,
+  emailDataToUse: emailDataReducer,
   galleryImgData: fetchImagesGalleryReducer,
+  galleryImagesData: galleryImagesReducer,
   allFetchExTimesData: fetchAllExcludedTimesReducer,
   fileDate: fileReducer,
+  firebaseUrlData: firebaseUrlReducer,
   headerData: headerReducer,
   excludedTimesData: excludedTimesReducer,
   opinionsData: fetchOpinionsReducer,
   openShopData: fetchOpenShopReducer,
   permissionData: fetchPermissionToRegisterReducer,
+  paymentsDataFetchShop: fetchPaymentsShopReducer,
+  paymentsDataFetchWebsite: fetchPaymentsWebsiteReducer,
+  paymentsDataShop: paymentsShopReducer,
+  paymentsDataWebsite: paymentsWebsiteReducer,
   resizeData: resizeWindowReducer,
   scrollData: scrollReducer,
   serviceData: fetchServicesReducer,
   serverErrorData: serverErrorReducer,
   singleExcludedTimeData: singleExcludedTimeReducer,
-  testimonialData: opinionsReducer,
+  opinionsDataToUse: opinionsDataReducer,
   userData: usersReducer,
   useAdminData: adminDataReducer,
 });
