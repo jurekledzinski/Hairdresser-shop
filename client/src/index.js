7b59328b731739f99e474e871f3c8b115e7f263c
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-05-20 12:35:14 +0200

    Move BrowserRouter to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 import App from "./App";
 import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <App />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
