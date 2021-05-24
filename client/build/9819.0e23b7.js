b9725f33d3b84f0459f2a4a10ad136e875f15e42
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-05-24 20:59:10 +0200

    Remove build folder

--- client/build/9819.0e23b7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkbarbershop=self.webpackChunkbarbershop||[]).push([[9819],{49819:function(n,e,t){"use strict";var u=t(67294),c=t(16550);t(66169);e.Z=function(){var n=(0,c.k6)();return u.createElement("button",{onClick:function(){return n.push("/")},className:"button-go-back"},"Go back")}},66169:function(){}}]);
