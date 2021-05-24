7134a6045d3ad34f9ef9e4030375374d65dfffa2
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-05-24 12:54:31 +0200

    Modify content security policy

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use(passport.session());
 app.use(function (req, res, next) {
   res.setHeader(
     "Content-Security-Policy",
-    "default-src 'self' firebasestorage.googleapis.com *.firebasestorage.googleapis.com mongodb.com *.mongodb.com stripe.com *.stripe.com; img-src * 'self' data: https:;font-src *; object-src 'self';script-src 'self' 'sha256-7nnKyr+RUZ9a44Hg3lYwjgkUx5VyFQwv2ZUhVw6N7J4=' paypal.com *.paypal.com;style-src 'self' 'unsafe-inline' fontawesome.com *.fontawesome.com fonts.google.com *.fonts.google.com fonts.googleapis.com *.fonts.googleapis.com;"
+    "default-src 'self' firebasestorage.googleapis.com *.firebasestorage.googleapis.com mongodb.com *.mongodb.com stripe.com *.stripe.com; img-src * 'self' data: https:;font-src *; object-src 'self';script-src 'self' 'sha256-7nnKyr+RUZ9a44Hg3lYwjgkUx5VyFQwv2ZUhVw6N7J4=' stripe.com *.stripe.com;style-src 'self' 'unsafe-inline' fontawesome.com *.fontawesome.com fonts.google.com *.fonts.google.com fonts.googleapis.com *.fonts.googleapis.com;"
   );
   next();
 });
