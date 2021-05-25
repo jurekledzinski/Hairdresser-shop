d315163ee3a09c7ffaa68c34d8ef0f6eadc1d0b8
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-05-25 16:51:36 +0200

    Change place of display message

--- client/src/components/others/admin/adminCustomHooks/useRemoveCanceledOrder.js
+++ client/src/components/others/admin/adminCustomHooks/useRemoveCanceledOrder.js
@@ -17,11 +17,11 @@ const useRemoveCanceledOrder = (idOrderCanceled, setIsOpenModal) => {
     const { data, status } = await deleteCanceledOrder(idOrderCanceled);
 
     if (status === 200) {
-      dispatch(addServerSuccessMessage(data.success, "removeAtTableAdmin"));
+      dispatch(addServerSuccessMessage(data.success, "default"));
       dispatch(removeCanceledOrder(idOrderCanceled));
       setIsOpenModal(false);
     } else {
-      dispatch(addServerErrorMessage(data.alert, "removeAtTableAdmin"));
+      dispatch(addServerErrorMessage(data.alert, "default"));
     }
   };
 
